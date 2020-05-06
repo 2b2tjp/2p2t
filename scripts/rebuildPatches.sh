36def68099b57d12251d6e2cbbfe3afbc6ffc469
Minecrell <minecrell@minecrell.net>
2020-05-06 11:31:08 +0200

    Speed up rebuilding patches and reduce diff

--- scripts/rebuildPatches.sh
+++ scripts/rebuildPatches.sh
@@ -4,21 +4,19 @@
 PS1="$"
 basedir="$(cd "$1" && pwd -P)"
 workdir="$basedir/work"
+source "$basedir/scripts/functions.sh"
 gitcmd="git -c commit.gpgsign=false -c core.safecrlf=false"
 
 echo "Rebuilding patch files from current fork state..."
-
+nofilter="0"
+if [ "$2" = "nofilter" ]; then
+    nofilter="1"
+fi
 function cleanupPatches {
     cd "$1"
     for patch in *.patch; do
         echo "$patch"
-        gitver=$(tail -n 2 "$patch" | grep -ve "^$" | tail -n 1)
-        diffs=$($gitcmd diff --staged "$patch" | grep -E "^(\+|\-)" | grep -Ev "(From [a-z0-9]{32,}|\-\-\- a|\+\+\+ b|.index)")
-
-        testver=$(echo "$diffs" | tail -n 2 | grep -ve "^$" | tail -n 1 | grep "$gitver")
-        if [ "x$testver" != "x" ]; then
-            diffs=$(echo "$diffs" | sed 'N;$!P;$!D;$d')
-        fi
+        diffs=$($gitcmd diff --staged "$patch" | grep --color=none -E "^(\+|\-)" | grep --color=none -Ev "(\-\-\- a|\+\+\+ b|^.index)")
 
         if [ "x$diffs" == "x" ] ; then
             $gitcmd reset HEAD "$patch" >/dev/null
@@ -39,10 +37,11 @@ function savePatches {
         echo "REBASE DETECTED - PARTIAL SAVE"
         last=$(cat "$basedir/$target/.git/rebase-apply/last")
         next=$(cat "$basedir/$target/.git/rebase-apply/next")
+        orderedfiles=$(find . -name "*.patch" | sort)
         for i in $(seq -f "%04g" 1 1 $last)
         do
             if [ $i -lt $next ]; then
-                rm ${i}-*.patch
+                rm $(echo "$orderedfiles{@}" | sed -n "${i}p")
             fi
         done
     else
@@ -51,13 +50,21 @@ function savePatches {
 
     cd "$basedir/$target"
 
-    $gitcmd format-patch --no-stat -N -o "$basedir/${what_name}-Patches/" upstream/upstream >/dev/null
+    $gitcmd format-patch --zero-commit --full-index --no-signature --no-stat -N -o "$basedir/${what_name}-Patches/" upstream/upstream >/dev/null
     cd "$basedir"
     $gitcmd add -A "$basedir/${what_name}-Patches"
-    cleanupPatches "$basedir/${what_name}-Patches"
+    if [ "$nofilter" == "0" ]; then
+        cleanupPatches "$basedir/${what_name}-Patches"
+    fi
     echo "  Patches saved for $what to $what_name-Patches/"
 }
 
 savePatches "$workdir/Spigot/Spigot-API" "Paper-API"
-savePatches "$workdir/Spigot/Spigot-Server" "Paper-Server"
+if [ -f "$basedir/Paper-API/.git/patch-apply-failed" ]; then
+    echo "$(color 1 31)[[[ WARNING ]]] $(color 1 33)- Not saving Paper-Server as it appears Paper-API did not apply clean.$(colorend)"
+    echo "$(color 1 33)If this is a mistake, delete $(color 1 34)Paper-API/.git/patch-apply-failed$(color 1 33) and run rebuild again.$(colorend)"
+    echo "$(color 1 33)Otherwise, rerun ./paper patch to have a clean Paper-API apply so the latest Paper-Server can build.$(colorend)"
+else
+    savePatches "$workdir/Spigot/Spigot-Server" "Paper-Server"
+fi
 ) || exit 1
